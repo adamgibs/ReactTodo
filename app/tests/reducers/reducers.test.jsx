526216b7a7ab8202fda2f7d85c14b7e14c0890b5
Adam Gibson <adamgibs@outlook.com>
2017-01-02 12:40:47 -0600

    Add reducers and test for AddTodo

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -2,6 +2,7 @@ var expect = require('expect');
 var df = require('deep-freeze-strict');
 var reducers = require('reducers');
 
+
 describe('Reducers', () => {
   describe('searchTextReducer', () => {
     it('should set searchText', () => {
@@ -23,6 +24,41 @@ describe('Reducers', () => {
       var res = reducers.showCompletedReducer(df(false), df(action));
 
       expect(res).toEqual(true);
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: "ADD_TODO",
+        text: "walk dog"
+      };
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggleTodo', () => {
+      var action = {
+        type: "TOGGLE_TODO",
+        id: 1
+      };
+
+      var todo =   [
+        {
+          id: 1,
+          text: '',
+          completed: true,
+          createdAt: 123,
+          completedAt: 125
+        }
+      ];
+
+      var res = reducers.todosReducer(df(todo), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
     });
   });
 });
