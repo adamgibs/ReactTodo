5371f298bc0617e8863188fe97ac073d7a3e9b48
Adam Gibson <adamgibs@outlook.com>
2017-01-16 07:44:40 -0600

    move firebase creds to environment variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -3,11 +3,11 @@ import firebase from 'firebase';
 try {
   // Initialize Firebase
   var config = {
-    apiKey: "AIzaSyB-tb5g_ovvCDbVPkFNZ-m9GjNMCwWHyl0",
-    authDomain: "gibson-todo-app.firebaseapp.com",
-    databaseURL: "https://gibson-todo-app.firebaseio.com",
-    storageBucket: "gibson-todo-app.appspot.com",
-    messagingSenderId: "625482773384"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
 
   firebase.initializeApp(config);
